bc0e3a96a63d2623f5c56b9045207876f323aea9
boruOrris <boru2526@protonmail.com>
2025-04-08 18:47:52 +0900

    前端登录页面完成

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    style="
+      width: 100vw;
+      height: 100vh;
+      overflow: hidden;
+      display: flex;
+      position: relative;
+    "
+  >
+    <!-- 背景图片容器 -->
+    <div style="flex: 1; position: absolute; top: 0; left: 0; z-index: -1">
+      <el-image
+        style="width: 100%; height: 100%; object-fit: cover"
+        fit="cover"
+        src="/images/BKimage.jpg"
+      ></el-image>
+    </div>
+
+    <!-- 欢迎标题 -->
+    <div class="welcome-title">
+      <div style="font-family: STLiti; font-size: 30px">欢迎来到 聆鸢茶馆</div>
+      <div style="margin-top: 10px; font-family: FZShuTi; font-size: 20px">
+        喝我这杯七星茶 分文不取
+      </div>
+      <div style="margin-top: 5px; font-family: FZShuTi; font-size: 20px">
+        听他摆那龙门阵 再饮三盅
+      </div>
+    </div>
+
+    <!-- 右侧卡片区域 -->
+    <div class="right-card">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+  z-index: 1; /* 确保标题位于前面 */
+}
+
+.right-card {
+  position: relative;
+  width: 400px;
+  z-index: 2; /* 确保卡片位于标题和背景上面 */
+  background: rgba(255, 255, 255, 0.4); /* 调整背景透明度 */
+  backdrop-filter: blur(10px); /* 增强模糊效果 */
+  border-top-left-radius: 20px; /* 只设置左上圆角 */
+  border-bottom-left-radius: 20px; /* 只设置左下圆角 */
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3); /* 增强阴影效果 */
+  padding: 20px;
+  /* 添加渐变背景，使过渡更平滑 */
+  background: linear-gradient(
+    135deg,
+    rgba(255, 255, 255, 0.3),
+    rgba(255, 255, 255, 0.6)
+  );
+  margin-left: auto; /* 让卡片右对齐 */
+}
+</style>
---
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -0,0 +1,94 @@
+<template>
+  <div style="text-align: center; margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div
+        style="
+          font-size: 25px;
+          font-weight: bold;
+          color: white;
+          text-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+          font-family: STSong;
+        "
+      >
+        登录
+      </div>
+      <div
+        style="
+          font-size: 14px;
+          color: white;
+          text-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+        "
+      >
+        在进入系统之前，请先输入用户名和密码进行登录
+      </div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form v-model="form">
+        <el-form-item>
+          <el-input
+            v-model="form.username"
+            maxlength="10"
+            type="text"
+            placeholder="用户名/邮箱"
+          >
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" maxlength="20" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="12" style="text-align: left">
+            <el-form-item>
+              <el-checkbox
+                v-model="form.remember"
+                label="Remember Me"
+                style="color: white; text-shadow: 0 2px 5px rgba(0, 0, 0, 0.6)"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12" style="text-align: right">
+            <el-link
+              style="
+                color: white;
+                text-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+                font-weight: bold;
+              "
+              >忘记密码？</el-link
+            >
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div style="margin-top: 40px">
+      <el-button style="width: 250px" type="success" plain>立即登录</el-button>
+    </div>
+    <el-divider>
+      <span style="font-size: 13px; color: grey">没有账号</span>
+    </el-divider>
+    <div>
+      <el-button style="width: 250px" type="warning" plain>
+        立即注册
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { User, Lock } from "@element-plus/icons-vue";
+
+const form = reactive({
+  username: "",
+  password: "",
+  remember: false,
+});
+</script>
+
+<style></style>
