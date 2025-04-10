8f9fd09e399fa1b1b7bb548c53dc98e1505b226e
boruOrris <boru2526@protonmail.com>
2025-04-10 19:11:11 +0900

    更新了nginx.conf 以及 axios请求的后端地址

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from 'axios'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://38.22.90.169:8081'
 
 const app = createApp(App)
 
